e5766cfdfee00f93fafc72bd8a49b48297940928
Tony <tgruenwald@alphabroder.com>
2024-08-06 16:35:45 -0400

    r

--- threadsy-vue-app/server/config/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define('SHOPIFY_LOG_FILE', dirname(__DIR__) . '/logs/shopify_debug.log');
--- threadsy-vue-app/src/utils/productUtils.js
+++
@@ -1,8 +0,0 @@
-// src/utils/productUtils.js
-export function getMetafieldValue(metafields, key) {
-  if (!Array.isArray(metafields) || !metafields) {
-    return '';
-  }
-  const metafield = metafields.find(m => m.key === key);
-  return metafield ? metafield.value : '';
-}
